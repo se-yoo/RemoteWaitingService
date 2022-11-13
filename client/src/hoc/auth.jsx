d49d333d4cc9c7b31067e912a89089ac450fec99
Yoo Si Eun <yoose2019@tukorea.ac.kr>
2022-11-13 14:58:24 +0900

    api 오류 수정 및 탭사이즈 수정

--- client/src/hoc/auth.jsx
+++ client/src/hoc/auth.jsx
@@ -2,29 +2,30 @@
 import React, { useEffect } from 'react';
 import { auth } from '../store/actions/user_actions';
 import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from 'react-router-dom';
 
 export default function (SpecificComponent, option, adminRoute = null) {
   function AuthenticationCheck(props) {
     let user = useSelector(state => state.user);
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     useEffect(() => {
       //To know my current status, send Auth request 
       dispatch(auth()).then(response => {
-        console.log(response);
         //Not Loggined in Status 
         if (!response.payload.isAuth) {
           if (option) {
-            props.history.push('/login')
+            
           }
           //Loggined in Status 
         } else {
           //supposed to be Admin page, but not admin person wants to go inside
           if (adminRoute && !response.payload.isAdmin) {
-            props.history.push('/')
+            navigate('/')
           } else {
             if (option === false) {
-              props.history.push('/')
+              navigate('/')
             }
           }
         }
