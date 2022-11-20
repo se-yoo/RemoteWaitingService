8a52a2173442ae5d1ccb56a79dbf123228913a7f
Yoo Si Eun <yoose2019@tukorea.ac.kr>
2022-11-20 16:01:02 +0900

    이벤트 공유 팝업 구현

--- client/src/pages/EventDetailPage/EventDetailPage.jsx
+++ client/src/pages/EventDetailPage/EventDetailPage.jsx
@@ -1,17 +1,29 @@
-import { Box } from '@mui/material';
-import React, { useMemo } from 'react';
+import { Box, DialogActions } from '@mui/material';
+import React, { useCallback, useMemo, useState } from 'react';
 import ActionButtons from '../../components/ActionButtons';
+import CommonDialog from '../../components/CommonDialog';
 import MenuTitle from '../../components/MenuTitle';
 import BasicInfo from './Sections/BasicInfo';
 import NoticeInfo from './Sections/NoticeInfo';
 import ParticipantInfo from './Sections/ParticipantInfo';
 import QuestionList from './Sections/QuestionList';
+import ShareDialogContent from './Sections/ShareDialogContent';
 
 const EventDetailPage = () => {
+  const [openDialogShare, setOpenDialogShare] = useState(false);
+
+  const handleClose = useCallback(() => {
+    setOpenDialogShare(false);
+  }, []);
+
+  const onClickShare = useCallback(() => {
+    setOpenDialogShare(true);
+  }, []);
+
   const buttons = useMemo(() => {
     return [
       { text: "삭제", color: "red" },
-      { text: "공유", width: 200, variant: "outlined" },
+      { text: "공유", width: 200, variant: "outlined", onClick: onClickShare },
       { text: "수정", width: 200 }
     ];
   }, []);
@@ -27,6 +39,15 @@ const EventDetailPage = () => {
         WrapComponent={Box}
         sx={{ mt: 6, display: "flex" , justifyContent: "end" }}
         buttons={buttons}
+      />
+      <CommonDialog
+        open={openDialogShare}
+        onClose={handleClose}
+        width={900}
+        title="이벤트 공유"
+        subText="생성한 이벤트의 참여 페이지를 공유합니다"
+        closable
+        ContentComponent={<ShareDialogContent />}
       />
     </div>
   );
